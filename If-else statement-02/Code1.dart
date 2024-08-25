201563399229d7f3cb3ae0f181cbf4c441f8be97
Pratiksha2405 <pratikshamete123.scoe.it@gmail.com>
2024-08-25 22:19:05 +0530

    Add files via upload

---
+++ If-else statement-02/Code1.dart
@@ -0,0 +1,8 @@
+void main(){
+  int ramSize =8;
+  if(ramSize>4){
+     print("can run a flutter project");
+  }else{
+    print("Cant run a flutter project");
+  }
+}
---
+++ If-else statement-02/Code2.dart
@@ -0,0 +1,17 @@
+void main(){
+  var x=3;
+  if(x==1){
+    print("Voilet");
+
+  }else if(x==2){
+    print("Indigo");
+  }else if(x==3){
+    print("Blue");
+  }else if(x==4){
+    print("Green");
+  }else if(x==5){
+    print("Yellow");
+  }else{
+    print("not matching");
+  }
+}
